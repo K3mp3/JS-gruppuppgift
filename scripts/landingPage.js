f8180cdc879670a97a7505f317c71a9ef073ae5b
Melvin Kempe <k3mp314@gmail.com>
2025-01-02 14:06:25 +0100

    [Add] styling and script files

---
+++ scripts/landingPage.js
@@ -0,0 +1,20 @@
+export const test = () => {
+  console.log("hej");
+};
+
+function init() {
+  menuOverlay = document.querySelector(".menu-overlay");
+
+  gsap.to(menuOverlay, { x: width, opacity: 0 });
+
+  /* Create eventlisteners */
+  window.addEventListener("resize", updateScreenSize);
+  window.addEventListener("scroll", changeNavColor);
+  window.addEventListener("resize", removeClass);
+
+  /* Calling functions */
+  createEventListeners();
+  removeClass();
+  checkScreenSize();
+  navAnimation();
+}
