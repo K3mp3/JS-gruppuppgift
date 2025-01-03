559d3083601db73115b352eb120a22d419e05f41
arwin azizi <arwin2002@hotmail.se>
2025-01-03 16:16:28 +0100

    created function to get users input into variable userInput and added display none for section landing2

---
+++ scripts/travelFrom.js
@@ -0,0 +1,9 @@
+function travelFrom(event) {
+
+    event.preventDefault();
+    const userInput = document.getElementById("user-input").value;
+    userInput;
+    document.getElementById('landing2').style.display = 'none';
+    document.getElementById('landing3').style.display = 'block'
+
+}
