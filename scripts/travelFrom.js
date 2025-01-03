765c0e010786f95a226b15bd7be7f938fdab840a
arwin azizi <arwin2002@hotmail.se>
2025-01-03 16:38:48 +0100

    changed the display settings syntax array style instead of id

--- scripts/travelFrom.js
+++ scripts/travelFrom.js
@@ -1,9 +1,9 @@
-function travelFrom(event) {
+const travelFrom = (event) => {
 
     event.preventDefault();
     const userInput = document.getElementById("user-input").value;
     userInput;
-    document.getElementById('landing2').style.display = 'none';
-    document.getElementById('landing3').style.display = 'block'
+    document.querySelectorAll('.landing-page')[1].style.display = 'none';
+    document.querySelectorAll('.landing-page')[2].style.display = 'flex'
 
 }
