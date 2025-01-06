e83edb2f79d13b14fa27c175c67509f48612ba87
arwin azizi <arwin2002@hotmail.se>
2025-01-06 20:43:23 +0100

    changed style.display to => classlist.add(hidden);

--- scripts/travelFrom.js
+++ scripts/travelFrom.js
@@ -3,7 +3,7 @@ const travelFrom = (event) => {
     event.preventDefault();
     const userInput = document.getElementById("user-input").value;
     userInput;
-    document.querySelectorAll('.landing-page')[1].style.display = 'none';
-    document.querySelectorAll('.landing-page')[2].style.display = 'flex'
+    document.querySelectorAll('.landing-page')[1].classList.add("hidden");
+    document.querySelectorAll('.landing-page')[2].classList.add("hidden");
 
 }
